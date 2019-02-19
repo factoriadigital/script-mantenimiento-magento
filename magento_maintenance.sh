625ad69351c18b414dc5d7907b6e94308018e471
Toni Cortés <tonicc77@gmail.com>
2019-02-19 13:21:50 +0000

    Revert "Exclude tar gz from new tar gz files and set maximum file size to include inside it"
    This reverts commit eb07f9e99d21fe533fe3879fc76f097c221a3982.

--- magento_maintenance.sh
+++ magento_maintenance.sh
@@ -10,7 +10,6 @@ SESSION_FILES_EXPIRATION=7 # In days
 CACHE_IMAGES_EXPIRATION=180 # In days
 LOG_VISITOR_EXPIRATION_DAYS=7 # In days
 ERROR_LOG_MONTH_DAY=25 #Month day when error_log file will be removed
-MAXIMUM_LOG_FILE_SIZE=50 # In MB
 
 # Function declarations
 function usage() 
@@ -30,7 +29,7 @@ function backup_logs()
         name=$(date '+%d-%m-%Y')
         fullname="logs_$name.tar.gz"
         cd $1/var/log
-        tar --exclude='*.tar.gz' -zcf $fullname $(find . -size -"$MAXIMUM_LOG_FILE_SIZE"M)
+        tar -zcf $fullname .
         show_log "$fullname successfully created on $1/var/log"        
     else
         show_log "Skipping backup logs process: $1/var/log/ folder does not exist"
